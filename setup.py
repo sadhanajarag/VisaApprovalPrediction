77d27b1903e28c774b108927a640002800d1541c
sadhanajarag <sadhana20.jaarg@gmail.com>
2026-02-23 21:20:23 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sadhana",
+    author_email="sadhana20.jarag@gmail.com",
+    packages=find_packages()
+)
