0c3432b7d75c0ffb85a2d0268db54f9914df3260
sadhanajarag <sadhana20.jaarg@gmail.com>
2026-03-01 15:22:13 -0500

    added data ingestion file

---
+++ us_visa/pipeline/training_pipeline.py
@@ -0,0 +1,52 @@
+import sys
+from us_visa.exception import USvisaException
+from us_visa.logger import logging
+from us_visa.components.data_ingestion import DataIngestion 
+from us_visa.components.data_validation import DataValidation
+from us_visa.components.data_transformation import DataTransformation
+#from us_visa.components.model_trainer import ModelTrainer
+#from us_visa.components.model_evaluation import ModelEvaluation
+#from us_visa.components.model_pusher import ModelPusher
+
+
+from us_visa.entity.config_entity import (DataIngestionConfig,
+                                          DataValidationConfig,
+                                          DataTransformationConfig,
+                                          ModelTrainerConfig,
+                                          ModelEvaluationConfig,
+                                          ModelPusherConfig)
+
+
+from us_visa.entity.artifact_entity import (DataIngestionArtifact,
+                                            DataValidationArtifact,
+                                            DataTransformationArtifact,
+                                            ModelTrainerArtifact,
+                                            ModelEvaluationArtifact,
+                                            ModelPusherArtifact)
+
+class TrainingPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
+
+
+def start_data_ingestion(self) -> DataIngestionArtifact:
+    """
+        This method of TrainPipeline class is responsible for starting data ingestion component
+    """
+    try :
+        logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+        logging.info("Getting the data from mongodb")
+
+        data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Got the train_set and test_set from mongodb")
+        logging.info("Exited the start_data_ingestion method of TrainPipeline class")
+
+        return data_ingestion_artifact
+    except Exception as e:
+        raise USvisaException(e,sys) from e
