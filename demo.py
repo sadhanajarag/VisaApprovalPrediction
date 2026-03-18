0a809c2a748b1e862a3390aa7e7d8d3564e5b4d9
sadhanajarag <sadhana20.jaarg@gmail.com>
2026-03-18 08:24:31 -0400

    Data Transformation DONE

--- demo.py
+++ demo.py
@@ -6,7 +6,11 @@ from us_visa.pipeline.training_pipeline import TrainingPipeline
 
 import os
 from pymongo import MongoClient
+import pandas as pd
+import numpy as np
 
+print(pd.__version__)
+print(np.__version__)
 #mongo_url = os.environ.get("MONGODB_URL")
 #client = MongoClient(mongo_url)
 #db = client.get_database("visa_db")
